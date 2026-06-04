5368299549be22b4d981fe0c642ae79172a85d98
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-06-05 00:51:55 +0300

    Create a grid of squares
    The grid was created to function as a canvas for drawings.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 const grid = document.querySelector("#grid")
-const square = document.createElement("div");
-square.classList.toggle("square");
-grid.appendChild(square);
+
+for (i = 1; i <= 16 * 16; i++) {
+  const square = document.createElement("div");
+  square.classList.toggle("square");
+  grid.appendChild(square);
+}
