688f24a937caf3e8a034ec2285369e25a29fbee6
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-06-05 15:30:01 +0300

    Add a button to renew the grid
    Previously, the page had to be reloaded to renew the grid. A button has
    been added for this.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,31 @@
-// A variable for a grid element
-const grid = document.querySelector("#grid")
-
-// A loop that adds squares to the grid
-for (i = 1; i <= 16 * 16; i++) {
-  const square = document.createElement("div");
-  square.classList.toggle("square");
-  grid.appendChild(square);
+// A function for creating a grid for drawing
+function gridConstructor(n) {
+  // A loop that adds squares to the grid
+  for (i = 1; i <= n * n; i++) {
+    let square = document.createElement("div");
+    square.style.width = `${960 / n}px`;
+    square.style.height = `${960 / n}px`;
+    square.classList.toggle("square");
+    grid.appendChild(square);
 }
 
-// An event listener that enables drawing on the grid
-grid.addEventListener("mouseover", (event) => {
-  let color = event.target;
-  color.classList.add("color");
+  // An event listener that enables drawing on the grid
+  grid.addEventListener("mouseover", (event) => {
+    let color = event.target;
+    color.classList.add("color");
+  });
+}
+
+// Variables for html elements
+const grid = document.querySelector("#grid");
+const btn = document.querySelector("button");
+
+// An event listener that renews the grid with a custom number of squares
+btn.addEventListener("click", () => {
+  grid.replaceChildren();
+  let n = prompt("Enter a number of squares per side (no more than 100).", "");
+  gridConstructor(n);
 });
+
+// The initial creation of a grid
+gridConstructor(16);
