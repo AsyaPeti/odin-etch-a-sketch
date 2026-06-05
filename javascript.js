ee7f008f5ab5406c6675c5727c07398fd38f4045
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-06-05 14:57:23 +0300

    Update the comments

--- javascript.js
+++ javascript.js
@@ -1,11 +1,14 @@
+// A variable for a grid element
 const grid = document.querySelector("#grid")
 
+// A loop that adds squares to the grid
 for (i = 1; i <= 16 * 16; i++) {
   const square = document.createElement("div");
   square.classList.toggle("square");
   grid.appendChild(square);
 }
 
+// An event listener that enables drawing on the grid
 grid.addEventListener("mouseover", (event) => {
   let color = event.target;
   color.classList.add("color");
